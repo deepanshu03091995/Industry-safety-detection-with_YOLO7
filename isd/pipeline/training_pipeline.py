68a672d33a3aedd0c256cad8c40eec03ced22ee1
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2024-03-28 17:56:25 +0530

    model training completed

--- isd/pipeline/training_pipeline.py
+++ isd/pipeline/training_pipeline.py
@@ -4,18 +4,20 @@ from isd.exception import isdException
 from isd.configuration.s3_operations import S3Operation
 from isd.components.data_ingestion import DataIngestion
 from isd.components.data_validation import DataValidation
+from isd.components.model_trainer import ModelTrainer
 
 
-from isd.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from isd.entity.config_entity import (DataIngestionConfig,DataValidationConfig,ModelTrainerConfig)
 
 
-from isd.entity.artifacts_entity import (DataIngestionArtifact,DataValidationArtifact)
+from isd.entity.artifacts_entity import (DataIngestionArtifact,DataValidationArtifact,ModelTrainerArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
         self.s3_operations = S3Operation()
 
 
@@ -66,7 +68,17 @@ class TrainPipeline:
         except Exception as e:
             raise isdException(e, sys) from e    
 
-    
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise isdException(e, sys)
 
     def run_pipeline(self) -> None:
         try:
@@ -74,6 +86,8 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
 
         except Exception as e:
             raise isdException(e, sys)
